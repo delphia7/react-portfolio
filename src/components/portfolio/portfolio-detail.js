0bf339a8e46c6cb1280268d6750b15ce2344bb2e
delphia7 <rosendolo78@gmail.com>
2020-03-10 23:00:42 -0400

    testing

---
+++ src/components/portfolio/portfolio-detail.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      portfolioItem:{}
+    }
+  }
+
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://delphiarose.devcamp.space/portfolio/portfolio_items/${
+          this.props.match.params.slug
+        }`, 
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.setState({
+          portfolioItem: response.data.portfolio_item
+        })
+      })
+      .catch(error => {
+        console.log("getportfolioitem error", error);
+      });
+  }
+
+  render() {
+    const {
+name,
+description,
+url,
+category,
+thumb_image_url,
+banner_image_url,
+logo_url
+    } = this.state.portfolioItem;
+
+    const bannerStyles = {
+      backgroundImage: "url(" + banner_image_url + ")",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center center"
+    };
+
+    const logoStyles = {
+      width: "200px"
+    };
+
+    return (
+      <div className="portfolio-detail-wrapper">
+        <div className="banner" style={bannerStyles}>
+          <img src={logo_url} style={logoStyles} />
+        </div>
+
+        <div className="portfolio-detail-description-wrapper">
+          <div className="description">{description}</div>
+        </div>
+
+        <div className="bottom-content-wrapper">
+          <a href={url} className="site-link" target="_blank">
+            visit {name}
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
